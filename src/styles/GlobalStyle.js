5c55a3131636c6c955edc9d106302af2dfe507af
vinivin153 <vinivin153@naver.com>
2023-04-23 17:57:42 +0900

    init

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video, input, textarea, select {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+
+
+button {
+  cursor: pointer;
+  border: none;
+  background: none;
+}
+
+* {font-family: 'Noto Sans KR';}
+`;
+
+export default GlobalStyle;
