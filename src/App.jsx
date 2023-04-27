66ef7512ba49e8aea9ab615215b37fecea61b4b6
vinivin153 <vinivin153@naver.com>
2023-04-27 22:32:48 +0900

    chore: 설정값 변경

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Header />
         <Routes>
           <Route path="/Home" element={<Home />}></Route>
